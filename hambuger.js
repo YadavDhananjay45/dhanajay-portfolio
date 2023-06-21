1b821af033bc20978e72582d6c031b5cd98eff6a
YadavDhanajay45 <dky99yadav@gmail.com>
2023-06-21 22:49:03 +0530

    added projects section and updated cursor trail

--- hambuger.js
+++ hambuger.js
@@ -6,22 +6,6 @@ bars.addEventListener("click", () => {
   }
   navMenu.classList.toggle("active");
 });
-const skillBox = document.querySelectorAll(".img-box");
-const textVis = document.querySelectorAll(".text-box");
-for (skill of skillBox) {
-  skill.addEventListener("mouseover", () => {
-    for (text of textVis) {
-      text.classList.toggle("active");
-    }
-  });
-}
-for (skill of skillBox) {
-  skill.addEventListener("mouseout", () => {
-    for (text of textVis) {
-      text.classList.toggle("active");
-    }
-  });
-}
 // const htmlBox = document.querySelector(".html-img");
 // const textVis = document.querySelector(".img-text1");
 // htmlBox.addEventListener("mouseover", () => {
